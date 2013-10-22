70b295e19e5f41e2e01aa56ee65cd77fff54cd95
Adam Wen <adamwen829@gmail.com>
2013-10-22 11:23:53 +0800

    修复命名风格

--- teacher/utils.py
+++ teacher/utils.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-from teachers.models import Teacher
+from teacher.models import Teacher
 
 def is_teacher(function, redirect_url=''):
     def wrap(request, *args, **kwargs):
@@ -11,6 +11,6 @@ def is_teacher(function, redirect_url=''):
             if redirect_url:
                 return redirect(redirect_url)
             return HttpResponse('hehe')
-    wrap.__doc__=function.__doc__
-    wrap.__name__=function.__name__
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
     return wrap
