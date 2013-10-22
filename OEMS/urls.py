b2d0898beea7e09de9237d3ed8c633a6d58f7a64
zyp <276291420@qq.com>
2013-10-22 21:36:01 +0800

    完成教师的“查看所有实验"功能

--- OEMS/urls.py
+++ OEMS/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from test1.views import two
+from experiment.views import display_experiment
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-	(r'^two/$', two),
+    url(r'^display-experiment/$', display_experiment, name="display_experiment_list"),
 )
--- experiment/views.py
+++ experiment/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.shortcuts import render
+
+from experiment.models import Experiment
+
+
+def display_experiment(request):
+    experiment_list = Experiment.objects.all()
+    return render(request, '../templates/experiment/display_experiments.html',
+                  {experiment_list: experiment_list})
+
