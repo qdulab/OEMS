6ae351d2ab37221292d5df756dd87a12378fd05c
WangFushu <wangfushu1992@gmail.com>
2013-10-22 12:08:41 +0800

    修改experiment目录下models.py中的代码缩进问题

--- experiment/models.py
+++ experiment/models.py
@@ -1,33 +1,32 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
 from teacher.models import Teacher
 
 class LessonCategory(models.Model):
-	name = models.CharField(max_length=60, null=False, blank=False)
-	created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=60, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		verbose_name = _('Lesson Category')
-		verbose_name_plural = _('Lesson Categories')
+    class Meta:
+        verbose_name = _('Lesson Category')
+        verbose_name_plural = _('Lesson Categories')
 
-	def __unicode__(self):
-		return u"Lesson Category:" % self.name
+    def __unicode__(self):
+        return u"Lesson Category:" % self.name
 
 
 class Lesson(models.Model):
-	category = models.ForeignKey(LessonCategory)
-	name = models.CharField(null=True, blank=False, max_length=64)
-	teacher = models.ForeignKey(Teacher)
-	status = models.BooleanField(blank=False)
-	info = models.TextField(null=True, blank=True)
-	create_at = models.DateTimeField(auto_now_add=True)
-	students = models.ManyToManyField(User)
+    category = models.ForeignKey(LessonCategory)
+    name = models.CharField(null=True, blank=False, max_length=64)
+    teacher = models.ForeignKey(Teacher)
+    status = models.BooleanField(blank=False)
+    info = models.TextField(null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(User)
 
-	class Meta:
-		verbose_name = _('Lesson')
-		verbose_name_plural = _('Lessons')
+    class Meta:
+        verbose_name = _('Lesson')
+        verbose_name_plural = _('Lessons')
 
-	def __unicode__(self):
-		return u"Lesson: " % self.name
+    def __unicode__(self):
+        return u"Lesson: " % self.name
