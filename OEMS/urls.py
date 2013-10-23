cd0ff9f46c6e8f58468f453bda59a4b455fbd549
Adam Wen <adamwen829@gmail.com>
2013-10-23 15:18:24 +0800

    删除污染代码

--- OEMS/urls.py
+++ OEMS/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from test1.views import two
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,5 +14,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-	(r'^two/$', two),
 )
