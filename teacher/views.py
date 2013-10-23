e919e4a9db351fe2e6c0203b857a073716c2bdcb
Wu Wenjie <wwjyt@me.com>
2013-10-23 18:40:48 +0800

    user_login修改 & 给首页增加url处理

--- teacher/views.py
+++ teacher/views.py
@@ -1,22 +1,26 @@
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, render
+from teacher.models import Teacher
 
 def user_login(request):
-
+  #  import pdb; pdb.set_trace()
     if request.method == 'POST':
-        username = request.POST.get['username']
-        password = request.POST.get['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
     	user = authenticate(username=username, password=password)
-        if user is not None:
+        if user is not None and user.is_active:
             login(request, user)
-            return render_to_response('../templates/side_bar.html', {'username':username}) #needChange: URL
+            if isinstance (user,Teacher):
+                return rende(request,'../templates/base.html', {'username':username}) #TODO URL->teacher.html
+            else:
+                return rende(request,'../templates/base.html', {'username':username}) #TODO URL->student.html
         else:
-            return redirect('index') #when the user not in SQL
+            return render(request,'../templates/base.html') #when the user not in SQL
     else:
-        return redirect('index') #when request.method is not POST
+        return render(request,'../templates/base.html') #when request.method is not POST
 
-@login_required()   #needChange: (login_url='/teacher/login'  or /strudent/login)
+@login_required()   #TODO  add:(login_url='/teacher/login'  or /strudent/login)
 def user_logout(request):
     logout(request)
-    return redirect('index')
+    return render(request,'../templates/base.html')
