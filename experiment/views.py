b630c96da1c88f90ee22491082961f0673cf19c5
zyp <276291420@qq.com>
2013-10-23 16:58:38 +0800

    增加教师的创建实验功能

--- experiment/views.py
+++ experiment/views.py
@@ -1,11 +1,16 @@
 # Create your views here.
 from django.shortcuts import render
-
+from teacher.utils import is_teacher
 from experiment.models import Experiment
-
+from django.contrib.auth.decorators import login_required
 
 def display_experiment(request):
     experiment_list = Experiment.objects.all()
     return render(request, '../templates/experiment/display_experiments.html',
                   {experiment_list: experiment_list})
 
+@login_required()
+@is_teacher(redirect_url='index')
+def create_experiment(request):
+
+    return render(request, '../templates/experiment/create_experiment.html')
