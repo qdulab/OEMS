942d6af5f5a7902adc7d32afb1ada27d1a08a2d2
zyp <276291420@qq.com>
2013-10-23 21:50:12 +0800

    增加教师的创建实验功能

--- experiment/views.py
+++ experiment/views.py
@@ -1,16 +1,30 @@
 # Create your views here.
 from django.shortcuts import render
 from teacher.utils import is_teacher
-from experiment.models import Experiment
+from experiment.models import Experiment, Lesson
 from django.contrib.auth.decorators import login_required
+import datetime
+
+
+def index(request):
+    return render(request, '../templates/two_columns.html')
 
 def display_experiment(request):
     experiment_list = Experiment.objects.all()
     return render(request, '../templates/experiment/display_experiments.html',
                   {experiment_list: experiment_list})
 
-@login_required()
-@is_teacher(redirect_url='index')
+#@login_required()
+#@is_teacher('index')
 def create_experiment(request):
-
-    return render(request, '../templates/experiment/create_experiment.html')
+    if request.method == 'POST' :
+        experiment_name = request.POST.get("experiment_name")
+        experiment_content = request.POST.get("experiment_content")
+        experiment_deadline = request.POST.get("deadline")
+        experiment_remark = request.POST.get("remark")
+        experiment_weight = request.POST.get("weight")
+        experiment_lesson = Lesson.objects.filter(id=request.POST.get("lesson_id"))
+        experiment = Experiment(name=experiment_name, content=experiment_content, deadline=experiment_deadline, remark=experiment_remark, weight=experiment_weight, lesson=experiment_lesson)
+        experiment.save()
+    else:
+        return render(request, '../templates/experiment/create_experiment.html')
--- teacher/utils.py
+++ teacher/utils.py
@@ -3,13 +3,13 @@ from django.shortcuts import redirect
 
 from teacher.models import Teacher
 
-def is_teacher(function, redirect_url=''):
+def is_teacher(function):
     """This is a view decorator, to valid if teacher has login
        login: call function as normal
        not login: rediretu to the redirect_url, if redirect_url is None,
                   will return a HttpResponse which body is "hehe"
     """
-    def wrap(request, *args, **kwargs):
+    def wrap(request, redirect_url=''):
         try:
             user = request.user
         except AttributeError:
