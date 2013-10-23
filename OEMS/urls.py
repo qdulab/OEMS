a7d5545e0cac2e5cc914b00fd7084ebc963f88f2
WangFushu <wangfushu1992@gmail.com>
2013-10-23 17:14:08 +0800

    完成创建科目

--- OEMS/urls.py
+++ OEMS/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^display-experiment/$', display_experiment, name="display_experiment_list"),
+    url(r'^create_lesson_category/$', 'experiment.views.create_lesson_category', name='create_lesson_category'),
 )
--- experiment/views.py
+++ experiment/views.py
@@ -1,8 +1,16 @@
-# Create your views here.
 from django.shortcuts import render
 
 from experiment.models import Experiment
+from experiment.models import LessonCategory, Lesson
 
+def create_lesson_category(request):
+    if request.method == 'POST':
+        data = request.POST
+        new_lesson_category = LessonCategory(name=data['lesson_category'])
+        new_lesson_category.save()
+        return render(request, 'experiment/create_succeed.html', {})
+    else:
+        return render(request, 'experiment/create_lesson_category.html', {})
 
 def display_experiment(request):
     experiment_list = Experiment.objects.all()
