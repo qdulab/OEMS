b14a4e4442541be1ede4958aeff98d7c8d83ea23
Wu Wenjie <wwjyt@me.com>
2013-10-23 18:54:55 +0800

    urls修改

--- OEMS/urls.py
+++ OEMS/urls.py
@@ -14,5 +14,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'teacher.views.user_login'),
+    url(r'^$', 'teacher.views.user_index'),
+    url(r'^$', 'teacher.views.user_index', name='index'),
+    url(r'^login/$','teacher.views.user_login', name='login'),
+    url(r'^logout/$', teacher.views.user_logout, name='logout'),
+
 )
