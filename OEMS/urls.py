606b4aff5dd875098b96074781a050dc960b51b7
WangFushu <wangfushu1992@gmail.com>
2013-10-24 15:42:14 +0800

    修改url.py，添加teacher前缀

--- OEMS/urls.py
+++ OEMS/urls.py
@@ -15,8 +15,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', 'experiment.views.index', name="index"), 
-    url(r'^display-experiment/$','experiment.views.display_experiment', name="display_experiment_list"),
-    url(r'^create_lesson_category/$', 'experiment.views.create_lesson_category', name='create_lesson_category'),
-    url(r'^create_experiment/$', 'experiment.views.create_experiment', name="create_experiment"),
-    url(r'^create_lesson/$', 'experiment.views.create_lesson', name='create_lesson'),
+    url(r'^teacher/display-experiment/$','experiment.views.display_experiment', name="display_experiment_list"),
+    url(r'^teacher/create_lesson_category/$', 'experiment.views.create_lesson_category', name='create_lesson_category'),
+    url(r'^teacher/create_experiment/$', 'experiment.views.create_experiment', name="create_experiment"),
+    url(r'^teacher/create_lesson/$', 'experiment.views.create_lesson', name='create_lesson'),
 )
