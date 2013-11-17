4823a8e72c529209595a658021f4444774bdf4b7
WangFushu <wangfushu1992@gmail.com>
2013-11-17 19:46:13 +0800

    添加创建课程科目的单元测试

--- experiment/tests.py
+++ experiment/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.http import HttpResponse
 from django.test import TestCase
+from django.test.client import Client
+
+from experiment.models import LessonCategory, Lesson
+from teacher.models import Teacher
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class LessonCategoryTest(TestCase):
+    def setUp(self):
+        self.teacher = Teacher(username='nihaoma')
+        self.teacher.set_password('wangfushu')
+        self.teacher.save()
+        self.client = Client()
+        self.client.login(username='nihaoma', password='wangfushu')
+        self.category = LessonCategory(name='existed')
+        self.category.save()
+
+    def test_create_lesson_category(self):
+        response = self.client.post('/teacher/category/create/',
+                                    {'name': "test"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "success")
+
+    def test_create_existed_category(self):
+        response = self.client.post('/teacher/category/create/',
+                                    {'name': "existed"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content,
+                         "Category has already existed")
+
