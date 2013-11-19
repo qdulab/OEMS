5d4a006bd12c5ec919c26777f23d8ac08c6aed66
Wu Wenjie <wwjyt@me.com>
2013-11-19 17:16:28 +0800

    测试教师修改课程

--- experiment/tests.py
+++ experiment/tests.py
@@ -62,28 +62,47 @@ class TeacherExperimentTest(TestCase):
         self.lesson.save()
 
     def test_create_experiment(self):
-        response = self.client.post(reverse('create_experiment',
-                                            args=(self.lesson.id,)),
-                                    {'name': 'name',
-                                     #'create_at': datetime(2013, 11, 19, 7, 53, 47, 540461),
-                                     'content': 'content',
-                                     'lesson': self.lesson,
-                                     #'deadline': datetime(2013, 11, 19, 7, 53, 47, 540461),
-                                     'remark': 'remark',
-                                     'weight': 1,})
-        #self.assertRedirects(response, reverse("success"))
+        response = self.client.post(
+            reverse('create_experiment', args=(self.lesson.id, )),
+            {'name': 'name',
+             'content': 'content',
+             'lesson': self.lesson,
+             #'deadline': '',
+             'remark': 'remark',
+             'weight': 1})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, "success")
         experiment = Experiment.objects.get(name='name')
-        #self.assertEqual(experiment.create_at, datetime(2013, 11, 19, 7, 53, 47, 540461))
         self.assertEqual(experiment.content, 'content')
         self.assertEqual(experiment.lesson, self.lesson)
-        #self.assertEqual(experiment.deadline, datetime(2013, 11, 19, 7, 53, 47, 540461))
+        #self.assertEqual(experiment.deadline, '')
         self.assertEqual(experiment.remark, 'remark')
         self.assertEqual(experiment.weight, 1)
 
+    def test_modified_experiment(self):
+        self.experiment = Experiment(name='name', lesson=self.lesson, weight=1)
+        self.experiment.save()
+        response = self.client.post(
+            reverse('experiment_modify', args=(self.experiment.id, )),
+            {'name': 'new_name',
+             'content':'new_content',
+             'deadline':'',
+             'remark':'new_remark',
+             'weight': 2})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "success")
+        experiment = Experiment.objects.get(name='new_name')
+        self.assertEqual(experiment.content, 'new_content')
+        #self.assertEqual(experiment.deadline, '')
+        self.assertEqual(experiment.remark, 'new_remark')
+        self.assertEqual(experiment.weight, 2)
+
+
+
+
+
 '''
-    def test_experiment_exist(self):
+    def test__exist(self):
         self.assertTrue(self.student.profile)
         self.assertEqual(self.student.profile.school_id, '')
         self.assertEqual(self.student.profile.grade, '')
@@ -95,22 +114,6 @@ class TeacherExperimentTest(TestCase):
 
 
     def test_delete_experiment(self):
-
-
-    def test_modified_experiment(self):
-     response = self.client.post(reverse('update_student_profile'),
-                                    {'school_id': 'school_id',
-                                     'grade': 'grade',
-                                     'major': 'major',
-                                     'class_num': 'class_num',
-                                     'phone_num': ''})
-        self.assertRedirects(response, reverse('student_profile'))
-        self.assertEqual(self.student.profile.school_id, 'school_id')
-        self.assertEqual(self.student.profile.grade, 'grade')
-        self.assertEqual(self.student.profile.major, 'major')
-        self.assertEqual(self.student.profile.class_num, 'class_num')
-        self.assertEqual(self.student.profile.phone_num, '')
-
 
     def test_modified_experiment_illegally(self):
     response = self.client.post(reverse('update_student_profile'),
